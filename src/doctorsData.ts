05abe96a04e317573e36900423b786d3666c304e
Abu Luqman <momenodayoj12@gmail.com>
2026-03-14 18:06:06 +0300

    غيرت اسم طبيب

--- src/doctorsData.ts
+++ src/doctorsData.ts
@@ -4,7 +4,7 @@
 export const doctorsList = [
     {
         id: 1,
-        name: "د. مؤمن عدي",
+        name: "د. مؤمن ",
         specialty: "طب وجراحة الفم والأسنان",
         degree: "ماجستير تجميل الأسنان وزراعتها",
         university: "جامعة بغداد",
